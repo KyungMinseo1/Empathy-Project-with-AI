30bea980cf654ed0e5d198b747da48b6f466a591
0Kyung Desktop <pegood0217@gmail.com>
2025-10-03 23:44:42 +0900

    Fix model change

--- prompt.py
+++ prompt.py
@@ -1,20 +1,17 @@
-from openai import OpenAI
+import google.generativeai as genai
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-client = OpenAI()
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
-def generate_situation(model_name, topic):
-  messages = [
-    {
-      'role' : 'user',
-      'content' : [
-        {
-          'type' : 'input_text',
-          'text' : f'''
+model = genai.GenerativeModel('gemini-2.5-flash-lite')
+
+def generate_situation(topic):
+  messages = f'''
           당신은 공감 수업의 훌륭한 보조자입니다. 당신의 역할은 특정 주제에 맞는 간단한 갈등상황을 제시하고, 이에 적합한 공감적 대안들을 선택지로 생성하는 것입니다.
-          선택지는 총 4개로 구성되며, 2개는 공감적 판단이 많이 포함된, 나머지 2개는 개인 위주 판단이 많이 포함된 선택지를 생성하면 됩니다.
+          선택지는 총 4개로 구성되며, 2개는 공감적 판단이 많이 포함된, 나머지 2개는 개인 위주 판단이 약간 더 포함된 선택지를 생성하면 됩니다.
           
           ** 주의 사항 **
           최종 출력 포맷은 리스트로 출력할 것 ex) ['상황', '선택지1', '선택지2', '선택지3', '선택지4']
@@ -32,18 +29,11 @@ def generate_situation(model_name, topic):
 
           주제 : {topic}\n
           '''
-        }
-      ]
-    }
-  ]
-
-  response = client.responses.create(
-        model = model_name,
-        input = messages
-    )
   
-  return response.output_text
+  response = model.generate_content(messages)
+  
+  return response.text
 
 if __name__=="__main__":
-  result = generate_situation('gpt-4o-mini', '지우개 훔치기')
+  result = generate_situation('지우개 훔치기')
   print(result)
